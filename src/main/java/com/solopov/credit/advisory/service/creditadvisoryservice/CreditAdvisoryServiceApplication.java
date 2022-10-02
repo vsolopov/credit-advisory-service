e600baf9888d77572cd792615579f147e680e2a8
vsolopov <vladyslav.solopov@gmail.com>
2022-10-02 03:06:06 +0300

    init commit

---
+++ src/main/java/com/solopov/credit/advisory/service/creditadvisoryservice/CreditAdvisoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solopov.credit.advisory.service.creditadvisoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAdvisoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditAdvisoryServiceApplication.class, args);
+    }
+
+}
